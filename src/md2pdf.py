9fcbfc602bf4768bbab413c1828476e02a3b7583
dontru <dontru@users.noreply.github.com>
2020-09-30 09:08:44 +0200

    Add styles

--- src/md2pdf.py
+++ src/md2pdf.py
@@ -1,6 +1,7 @@
 from optparse import OptionParser
+import re
 
-from reportlab.lib.styles import StyleSheet1, ParagraphStyle
+from reportlab.lib.styles import StyleSheet1, ParagraphStyle, ListStyle
 from reportlab.pdfbase.pdfmetrics import registerFont, registerFontFamily
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import SimpleDocTemplate
@@ -20,14 +21,63 @@ registerFontFamily("", "DejaVuSans", "DejaVuSans-Bold", "DejaVuSans-Oblique", "D
 
 FONT_NAME = "DejaVuSans"
 FONT_SIZE = 12
+BORDER_COLOR = "#bbbbbb"
 
 styles = StyleSheet1()
 styles.add(ParagraphStyle("std", fontName=FONT_NAME, fontSize=FONT_SIZE))
+styles.add(ParagraphStyle("h3", fontName=f"{FONT_NAME}-Bold", fontSize=14))
+styles.add(ListStyle(
+    "list", fontName=FONT_NAME, fontSize=FONT_SIZE,
+    bulletType="bullet", bulletFontName=FONT_NAME, bulletFontSize=FONT_SIZE,
+))
+table_style_base = [
+    ('FONTNAME', (0, 0), (-1, -1), FONT_NAME),
+    ('FONTSIZE', (0, 0), (-1, -1), FONT_SIZE),
+    ('VALIGN', (0, 0), (-1, -1), 'MIDDLE'),
+    ('GRID', (0, 0), (-1, -1), 0.1, BORDER_COLOR)
+]
 
-document = SimpleDocTemplate(FILENAME + ".pdf", title=TITLE)
-flowables = []
+T_LIST = "list"
+T_LIST_ITEM = "list_item"
+T_TABLE = "table"
+T_TABLE_ROW = "table_row"
+T_PAGE_BREAK = "page_break"
+T_H3 = "h3"
+T_PARAGRAPH = "paragraph"
 
-with open(FILENAME) as f:
-    pass
+MATCHES = [
+    (T_LIST_ITEM, re.compile("^- (.*)$")),
+    (T_TABLE_ROW, re.compile("(^\\|(.*)\\|)$")),
+    (T_PAGE_BREAK, re.compile("^<!-- (PAGE_BREAK) -->$")),
+    (T_H3, re.compile("^### (.+)$")),
+    (T_PARAGRAPH, re.compile("^(.*)$")),
+]
 
-document.build(flowables)
+TABLE_MATCHES = [
+    ("LEFT", re.compile("^:?-+$")),
+    ("CENTER", re.compile("^:-+:$")),
+    ("RIGHT", re.compile("^-+:$")),
+]
+
+
+def main():
+    document = SimpleDocTemplate(FILENAME + ".pdf", title=TITLE)
+    flowables = []
+
+    with open(FILENAME) as f:
+        pass
+
+    document.build(flowables)
+
+
+def line2token(line):
+    for token_type, pattern in MATCHES:
+        search = re.search(pattern, line)
+        if search:
+            return token_type, search.group(1)
+    else:
+        raise
+
+
+if __name__ == '__main__':
+    main()
